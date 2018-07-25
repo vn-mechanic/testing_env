14eb2517b97de4285a3f8d911a4663680e4d0297
Alex Pro <dedifferentiator@gmail.com>
2018-07-25 13:00:33 +0300

    update 4.5

--- Main.py
+++ Main.py
@@ -12,11 +12,7 @@ def send_welcome(message):
         bot.send_message(119637031, message.text)
         bot.send_message(119637031, message.from_user)
     if "МАРІК" in str(message.text.upper()) or "МАРИК" in str(message.text.upper()):
-        if message.from_user.username == "mnstrlia" or message.from_user.username == "@d15hw45h3r":
-            bot.send_message(message.chat.id, "@mnstrlia, хочеш тортика?")
-        else:
-            random_text = sample(("Дададада", "Кого сьогодні поганяємо?","Це легко розв\'язується методом честної корупції"), 1)
-            bot.send_message(message.chat.id, random_text)
+        bot.send_message(message.chat.id, "@mnstrlia, хочеш тортика?") if message.from_user.username == "mnstrlia" or message.from_user.username == "@d15hw45h3r" else bot.send_message(message.chat.id, sample(("Дададада", "Кого сьогодні поганяємо?","Це легко розв\'язується методом честної корупції"), 1))
 
 '''@server.route("/"+token, methods=['POST'])
 def getMessage():
