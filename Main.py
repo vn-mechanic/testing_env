502e749ab9affa27ac1a5c30fc99f6350dbe3e0a
Alex Pro <dedifferentiator@gmail.com>
2018-07-25 12:53:26 +0300

    update 4.1

--- Main.py
+++ Main.py
@@ -2,15 +2,34 @@ from random import sample
 import telebot
 import os
 
-token = os.environ['token']
-bot = telebot.TeleBot(token)
+#from flask import Flask, request
+
+
+#server = Flask(__name__)
+
+#token = os.environ['token']
+bot = telebot.TeleBot('668673628:AAFtDeSMCjXp7xioE2retTrRqYfC-q2IQxo')
 list_of_participants = []
 
 @bot.message_handler(content_types=["text"])
 def send_welcome(message):
-    if message.chat.id != 119637031: bot.send_message(119637031, message.text);bot.send_message(119637031, message.from_user)
+    #print(message.text.upper())
+    print(message.from_user)
+    if message.chat.id != 119637031:
+        bot.send_message(119637031, message.text)
+        bot.send_message(119637031, message.from_user)
+
     if "МАРІК" in str(message.text.upper()) or "МАРИК" in str(message.text.upper()):
-        bot.send_message(message.chat.id, "@mnstrlia, хочеш тортика?") if message.from_user.username == "mnstrlia" or message.from_user.username == "@d15hw45h3r" else random_text = sample(("Дададада", "Кого сьогодні поганяємо?","Це легко розв\'язується методом честної корупції"), 1); bot.send_message(message.chat.id, random_text)
+       # with open(r'data.txt', "a+", encoding="utf-8") as data_add:
+           # data_add.write((str(message.from_user))+"\n")
+        if message.from_user.username == "mnstrlia" or message.from_user.username == "@d15hw45h3r":
+            bot.send_message(message.chat.id, "@mnstrlia, хочеш тортика?")
+        else:
+            random_text = sample(("Дададада", "Кого сьогодні поганяємо?",
+                                  "Це легко розв\'язується методом честної корупції"), 1)
+            #print(random_text)
+            #print("check")
+            bot.send_message(message.chat.id, random_text)
 
 '''@server.route("/"+token, methods=['POST'])
 def getMessage():
