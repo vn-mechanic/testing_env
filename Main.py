675c4d6603b7e885d8028c99ca4ca30eae3f398a
Alex Pro <dedifferentiator@gmail.com>
2018-09-13 12:58:50 +0300

    update 4.8

--- Main.py
+++ Main.py
@@ -3,18 +3,20 @@ import telebot
 import os
 
 token = os.environ['token']
-chachat = os.environ['chachat']
 bot = telebot.TeleBot(token)
 list_of_participants = []
 
 @bot.message_handler(content_types=["text"])
 def send_welcome(message):
-    if message.chat.id != chachat:
-        bot.send_message(chachat, message.text)
-        bot.send_message(chachat, message.from_user)
+    if message.chat.id != 119637031:
+        bot.send_message(119637031, message.text)
+        bot.send_message(119637031, message.from_user)
     if "МАРІК" in str(message.text.upper()) or "МАРИК" in str(message.text.upper()):
-        bot.send_message(message.chat.id, "@mnstrlia, хочеш тортика?") if message.from_user.username == "mnstrlia" or message.from_user.username == "@d15hw45h3r" else bot.send_message(message.chat.id, sample(("Дададада", "Кого сьогодні поганяємо?","Це легко розв\'язується методом честної корупції"), 1))
-
+        bot.send_message(message.chat.id, "@mnstrlia, хочеш тортика?") if message.from_user.username == "mnstrlia" else bot.send_message(message.chat.id, sample(("Дададада", "Кого сьогодні поганяємо?","Це легко розв\'язується методом честної корупції"), 1))
+        if message.from_user.username == "d15hw45h3r":
+            random_text = sample(("Хочеш тортика? виддай 27 грывен)())00)", "Чому тебе сьогодні не було на парі?",
+                                  "Лєра го в лс"), 1)
+            bot.reply_to(message, random_text)
 '''@server.route("/"+token, methods=['POST'])
 def getMessage():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
