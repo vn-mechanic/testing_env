da3d40c5f1bb7573b54c3388a7700d9812e30c1f
Alex Pro <dedifferentiator@gmail.com>
2018-10-02 16:33:25 +0300

    some things fixed

--- Main.py
+++ Main.py
@@ -3,6 +3,10 @@ import telebot
 import os
 
 token = os.environ['token']
+ania = os.environ['ania']
+lera = os.environ['lera']
+murchik = os.environ['murchik']
+rymlianyn = os.environ['rymlianyn']
 bot = telebot.TeleBot(token)
 
 
@@ -15,7 +19,7 @@ def send_welcome(message):
 
     if "МАРІК" in str(message.text.upper()) or "МАРИК" in str(message.text.upper()) or "MARIK" in str(message.text.upper()):
 
-        if message.from_user.id == 392901615: # ania.me script
+        if message.from_user.id == ania: # ania.me script
             random_number_0 = randint(0, 1)
             if random_number_0 == 0:
                 random_number = randint(0, 1)
@@ -32,7 +36,7 @@ def send_welcome(message):
                      "Це легко розв\'язується методом честної корупції"), 1)
                 bot.reply_to(message, random_text)
 
-        elif message.from_user.id == 382410466: # d15hw45h3r script
+        elif message.from_user.id == lera: # d15hw45h3r script
             random_number = randint(-10, 10)
             random_text = sample(("Хочеш тортика? виддай 60 грывен)())00)", "Чому тебе сьогодні не було на парі?",
                                   "Лєра го в лс", "Лєра, підеш завтра до дошки?","Дададада", "Кого сьогодні поганяємо?",
@@ -41,7 +45,7 @@ def send_welcome(message):
                                   "Ще одне повідомлення і я тобі поставлю "+str(random_number),), 1)
             bot.reply_to(message, random_text)
 
-        elif message.from_user.id == 372106864: # Murchik script
+        elif message.from_user.id == murchik: # Murchik script
             random_number = randint(-10, 10)
             random_text = sample(("Мурчик лох", "Мурчик, ща получиш",
                                   "Тікай з села, ща покличу Пашу", "Мурчик, задача на +6/-20.\n Ти лох?",
@@ -51,7 +55,7 @@ def send_welcome(message):
                                   "Підеш наступного понеділка до дошки?"), 1)
             bot.reply_to(message, random_text)
 
-        elif message.from_user.id == 405633723: # Rymlianyn script
+        elif message.from_user.id == rymlianyn: # Rymlianyn script
             random_number = randint(-10,10)
             random_text = sample(("Борис, з моїми лабками барісь", "Борис, погнали задачу пвп +5/-1",
                                   "Ще одне повідомлення і я тобі поставлю "+str(random_number),
